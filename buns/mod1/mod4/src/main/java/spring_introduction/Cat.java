1fd5678b15e82ccf5daa6639d2543b49956c8839
liroan <53300900+liroan@users.noreply.github.com>
2023-11-11 11:46:57 +0500

    Add files via upload

---
+++ buns/mod1/mod4/src/main/java/spring_introduction/Cat.java
@@ -0,0 +1,10 @@
+package spring_introduction;
+
+import org.springframework.stereotype.Component;
+
+public class Cat implements Pet {
+    @Override
+    public void say() {
+        System.out.println("may");
+    }
+}
